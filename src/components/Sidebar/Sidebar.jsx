1e1cfee711f65ba1574eaf2e52941782e33bb96d
Anushka Gupta <anushkagupta319@example.com>
2024-05-24 18:44:40 +0530

    fc

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -38,13 +38,14 @@ const Sidebar = () => {
           <img src={assets.history_icon} alt="history_icon" />
           {extended ? <p>Activity</p> : null}
         </div>
+        
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="setting_icon" />
           {extended ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
-    
+
   );
 };
 
